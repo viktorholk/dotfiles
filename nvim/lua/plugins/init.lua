4bc79f6e1952a4c96535f8a417486435134ccf2d
viktorholk <holkinator@gmail.com>
2023-02-17 12:48:16 +0100

    plen

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,7 +1,8 @@
 return {
 
-  { 'rebelot/kanagawa.nvim', lazy = false, config = function() vim.cmd([[colorscheme kanagawa]]) end },
+  { 'nvim-lua/plenary.nvim' },
 
+  { 'rebelot/kanagawa.nvim', lazy = false, config = function() vim.cmd([[colorscheme kanagawa]]) end },
   { 'goolord/alpha-nvim', config = function() require 'alpha'.setup(require 'alpha.themes.startify'.config) end },
   { 'kyazdani42/nvim-web-devicons', config = true },
   { 'windwp/nvim-autopairs', config = true },
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
 local M = {
   'nvim-telescope/telescope.nvim',
   dependencies = {
-    'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
   }
 }
