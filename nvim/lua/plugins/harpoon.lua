da6f488e754ba0a4c805deef9847d1700146a6dd
viktorholk <holkinator@gmail.com>
2024-03-19 08:03:51 +0100

    added hotkeys for 4 harpoon files

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -13,10 +13,10 @@ function M.config()
   vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
   vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-  --vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-  --vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-  --vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-  --vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+  vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+  vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+  vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+  vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
 
   -- Toggle previous & next buffers stored within Harpoon list
   vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
