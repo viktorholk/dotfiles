99504aba2694bf9783afc721fc1e93cc765d555f
viktorholk <holkinator@gmail.com>
2024-04-25 17:40:07 +0200

    added ruby to copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -5,6 +5,15 @@ return {
       require('copilot').setup({
         suggestion = { enabled = false },
         panel = { enabled = false },
+        filetypes = {
+          javascript = true, -- allow specific filetype
+          typescript = true, -- allow specific filetype
+          rust = true,
+          ruby = true,
+          lua = true,
+          ["*"] = false,     -- disable for all other filetypes and ignore default `filetypes`
+        },
+
       })
     end
   },
