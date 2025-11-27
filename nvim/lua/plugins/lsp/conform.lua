aaf1c3894386d3abdde2032a74e191c151f50105
Viktor Holk <viktor@holk.solutions>
2025-11-27 12:40:01 +1000

    Fixed formatter. added biome formatter. fallback to lsp

--- nvim/lua/plugins/lsp/conform.lua
+++ nvim/lua/plugins/lsp/conform.lua
@@ -2,9 +2,15 @@ local M = {}
 
 function M.setup()
   require("conform").setup({
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
     formatters_by_ft = {
-      javascript = { "prettierd", "prettier" },
-      typescript = { "prettierd", "prettier" },
+      javascript = { "biome" },
+      javascriptreact = { "biome" },
+      typescript = { "biome" },
+      typescriptreact = { "biome" },
+      json = { "biome" },
       ruby = { "rubocop" },
     },
     formatters = {
--- nvim/lua/plugins/lsp/handlers.lua
+++ nvim/lua/plugins/lsp/handlers.lua
@@ -6,14 +6,15 @@ local nnoremap = mappings.nnoremap
 local M = {}
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
-M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
+M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 M.flags = {
   debounce_text_changes = 150
 }
 
-M.on_attach = function(_client, bufnr)
+M.on_attach = function(client, bufnr)
+  client.server_capabilities.documentFormattingProvider = true
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- LSP Mappings.
@@ -22,8 +23,10 @@ M.on_attach = function(_client, bufnr)
   vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, { desc = "Hover", silent = true, buffer = bufnr })
   vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, { desc = "Find References", silent = true, buffer = bufnr })
   vim.keymap.set("n", "<leader>lc", vim.lsp.buf.code_action, { desc = "Code Action", silent = true, buffer = bufnr })
-  vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format({ async = true }) end,
-    { desc = "Format", silent = true, buffer = bufnr })
+vim.keymap.set("n", "<leader>lf", function()
+  require("conform").format({ async = true })
+end, { desc = "Format", silent = true, buffer = bufnr })
+
 end
 
 M.setup = function()
